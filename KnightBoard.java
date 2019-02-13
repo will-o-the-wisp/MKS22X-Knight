6335864e8e17c86d69ddb6e52da15017e4ce0ea7
will-o-the-wisp <kli18@stuy.edu>
2019-02-13 10:40:08 -0500

    wrote and tested toString

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,24 +1,49 @@
 public class KnightBoard{
+  private int[][] board;
+  public static void main(String[] args){
+    KnightBoard b = new KnightBoard(3,5);
+    System.out.println(b);
+  }
+  //@throws IllegalArgumentException when either parameter is negative.
   public KnightBoard(int startingRows,int startingCols){
-
+    board = new int[startingRows][startingCols];
   }
   public String toString(){
-    return "";
+    String ans = "";
+    for(int i=0;i<board.length;i++){
+      for(int j=0;j<board[0].length;j++){
+        if(board[i][j]==0){
+          ans+=" _";
+        }
+        else if(board[i][j]<10){
+          ans+=" "+board[i][j];
+        }
+        else{
+          ans+=board[i][j];
+        }
+        ans+=" ";
+      }
+      ans+="\n";
+    }
+    return ans;
   }
   /*
   @throws IllegalStateException when the board contains non-zero values.
-@throws IllegalArgumentException when either parameter is negative
- or out of bounds.
- */
- public boolean solve(int startingRow, int startingCol){
-   return false;
- }
- /*
- @throws IllegalStateException when the board contains non-zero values.
- @throws IllegalArgumentException when either parameter is negative
- or out of bounds.
- */
- public int countSolutions(int startingRow, int startingCol){
-   return 0;
- }
+  @throws IllegalArgumentException when either parameter is negative
+  or out of bounds.
+  */
+  public boolean solve(int startingRow, int startingCol){
+    return false;
+  }
+  private boolean solveH(int row, int col, int level){
+    return false;
+  }
+  /*
+  @throws IllegalStateException when the board contains non-zero values.
+  @throws IllegalArgumentException when either parameter is negative
+  or out of bounds.
+  */
+  public int countSolutions(int startingRow, int startingCol){
+    return 0;
+  }
 }
