bc59851b144d3569ee0fffd99571f1da5d9fee7e
will-o-the-wisp <kli18@stuy.edu>
2019-02-13 10:23:48 -0500

    created skeleton

---
+++ KnightBoard.java
@@ -0,0 +1,24 @@
+public class KnightBoard{
+  public KnightBoard(int startingRows,int startingCols){
+
+  }
+  public String toString(){
+    return "";
+  }
+  /*
+  @throws IllegalStateException when the board contains non-zero values.
+@throws IllegalArgumentException when either parameter is negative
+ or out of bounds.
+ */
+ public boolean solve(int startingRow, int startingCol){
+   return false;
+ }
+ /*
+ @throws IllegalStateException when the board contains non-zero values.
+ @throws IllegalArgumentException when either parameter is negative
+ or out of bounds.
+ */
+ public int countSolutions(int startingRow, int startingCol){
+   return 0;
+ }
+}
